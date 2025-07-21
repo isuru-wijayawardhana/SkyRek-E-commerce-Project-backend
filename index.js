2efc2935eb7be6b8bed08989297298e839d48956
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-07-21 18:54:48 +0530

    Add 'api' to URL

--- index.js
+++ index.js
@@ -53,9 +53,8 @@ mongoose.connect(connectionString).then(
 )
 
 
-app.use("/users",userRouter)
-app.use("/users/login",userRouter)
-app.use("/products",productRouter)
+app.use("/api/users",userRouter)
+app.use("/api/products",productRouter)
 
 app.listen(5000 , ()=>{
     console.log("Server is running on port 5000") 
