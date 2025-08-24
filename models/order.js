69ea70e6fd87b095ad93ba2f8ce0098baf1f1b03
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-08-24 14:12:54 +0530

    Day 18 Create Order schema

---
+++ models/order.js
@@ -0,0 +1,66 @@
+import { request } from "express";
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    orderId : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    name : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : Number,
+        required : true
+    },
+    status : {
+        type : String,
+        default : "prnding"
+    },
+    date : {
+        type : Date,
+        default : Date.now
+    },
+    items : [
+        {
+            productId : {
+                type : String,
+                required : true
+            },
+            name : {
+                type : String,
+                required : true
+            },
+            image : {
+                type : String,
+                required : true
+            },
+            price : {
+                type : Number,
+                required : true
+            },
+            qty : {
+                type : Number,
+                required : true
+            }
+        }
+    ],
+    notes : {
+        type : String,
+        default : "No additional notes"
+    }
+})
+
+const Order = mongoose.model("Order",orderSchema)
+
+export default Order;
