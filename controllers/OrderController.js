23d42bb7dec4e15c92be99c58f905db7b6062f63
Isuru Wijayawardhana <isuruanuranga@gmail.com>
2025-08-24 23:28:03 +0530

    Day 18 Create ordercontroller

---
+++ controllers/OrderController.js
@@ -0,0 +1,71 @@
+import Order from "../models/order.js"
+import Product from "../models/product.js"
+
+export async function createOrder(req,res) {
+    try {
+    if(req.user == null){
+        res.status(401).json({ message: "Please login to create order"})
+        return
+    }
+    
+    const latestOrder = await Order.find().sort({ date : -1 }).limit(1)
+
+    let orderId = "CBC00202"
+
+    if(latestOrder.length>0){
+        const lastOrderIdINString   =   latestOrder[0].orderId
+        const lastOrderIdWithoutPrefix = lastOrderIdINString.replace("CBC","")
+        const lastOrderIdINInteger  = parseInt(lastOrderIdWithoutPrefix)
+        const newOrderIdInInteger = lastOrderIdINInteger + 1
+        const newOrderIdWithoutPrefix = newOrderIdInInteger.toString().padStart(5,'0')
+        orderId = "CBC"+newOrderIdWithoutPrefix
+    }
+
+    const items = []
+    let total = 0
+
+    if(req.body.items !== null && Array.isArray(req.body.items)){
+        for(let i=0;i<req.body.items.length;i++){
+            let item = req.body.items[i]
+
+            let product = await Product.findOne({
+                productId : item.productId
+            })
+            if(product == null){
+                res.status(400).json({ message:"Invalid product ID" + item.productId})
+                return
+            }
+            item = {
+                productId : product.productId,
+                name: product.name,
+                image : product.images[0],
+                price : product.price,
+                qty : item.qty,
+            }
+            total += product.price * item.qty
+        }
+    }else{
+        res.status(400).json({ message: "Invalid items format"})
+    }
+
+    const order = new Order({
+        orderId : orderId,
+        email : req.user.email,
+        name : req.user.firstName + " " + req.user.lastName,
+        address : req.body.address,
+        phone : req.body.phone,
+        items : items,
+        total : total
+    })
+
+    const result = await order.save()
+
+    res.json({
+        message: "order create successfully",
+        order: result,
+    })
+}catch(error) {
+    console.error("Error creating order:",error)
+    res.status(500).json({ message: "Failed to create order" })
+}
+}
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import jwt from "jsonwebtoken"
 import productRouter from "./routers/productRouter.js";
 import dotenv from "dotenv"
 import cors from "cors"
+import orderRouter from "./routers/orderRouter.js";
 dotenv.config()
 
 
@@ -60,6 +61,7 @@ mongoose.connect(connectionString).then(
 
 app.use("/api/users",userRouter)
 app.use("/api/products",productRouter)
+app.use("/api/orders",orderRouter)
 
 app.listen(5000 , ()=>{
     console.log("Server is running on port 5000") 
